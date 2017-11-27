0e1521e52d30a29ce01f2622b39a9d97110b6dff
Sakata Yu <imanity.fefe@gmail.com>
2017-11-27 13:43:20 +0900

    initial_commit

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,14 @@
 $(function() {
     var socket = io();
     var myname;
+    var myId;
     var input_users = {};
+    var isTyping = false;
+    var private_id;
 
     $('form').submit(function() {
         if($('#m').val() != "") {
-            socket.emit('chat message', $('#m').val());
+            socket.emit('chat message', myname + ": " + $('#m').val());
             $('#m').val('');
         }
         return false;
@@ -13,32 +16,72 @@ $(function() {
 
     socket.on('chat message', function(msg) {
         $('#messages').append($('<li>').text(msg));
+        $('#main').scrollTop($('#main')[0].scrollHeight);
     });
 
-    socket.on('login', function(name) {
-        var name_data = name.split(":");
-        $('#users').append($('<li data-id="'+ name_data[1] +'" class="user">').text(name_data[0]));
-        $('#messages').append($('<li class="info">').text(name_data[0] + "さんが入室しました。"));
+    socket.on('login', function(users) {
+        $('#users').empty();
+        console.log(users);
+        Object.keys(users).forEach(function (key) {
+            if(key == "current" && users["current"] != myname) {
+                $('#messages').append($('<li class="info">').text(users["current"] + "さんが入室しました。"));
+            }else {
+                if(users[key] != myname) {
+                    $('#users').append($('<li data-id="'+ key +'" class="user">').text(users[key]));
+                    console.log(users[key]);
+                }else {
+                    myId = key;
+                }
+            }
+        });
+        $(".user").on('click', function() {
+            private_id = $(this).data("id");
+            console.log(id);
+            $("#private_name").text($(this).text());
+            $("#private_header").click();
+        });
     });
 
     socket.on('logout', function(id) {
-        $('#messages').append($('<li class="info">').text(($('.user[data-id="'+ id +'"]').text()) + "さんが退出しました。"));
-        $('.user[data-id="'+ id +'"]').remove();
+        if($('.user[data-id="'+ id +'"]').text() != "") {
+            $('#messages').append($('<li class="info">').text(($('.user[data-id="'+ id +'"]').text()) + "さんが退出しました。"));
+            $('.user[data-id="'+ id +'"]').remove();
+        }
     });
 
     socket.on('input start', function(data) {
         var name_data = data.split(':');
         input_users[name_data[1]] = name_data[0];
         var names = "";
+        var name_num = 0;
         Object.keys(input_users).forEach(function (key) {
-          names += input_users[key] + ",";
+            if(input_users[key] != myname && input_users[key] != undefined) {
+                names += input_users[key] + ",";
+                name_num++;
+            }
         });
-        $('.input_now').text(names);
+        if(name_num != 0) {
+            names = names.slice(0, -1) + "が入力しています。";
+            $('.input_now').text(names);
+        }
     });
 
     socket.on('input end', function(data) {
         var name_data = data.split(':');
         delete input_users[name_data[1]];
+        var name_num = 0;
+        var names;
+        Object.keys(input_users).forEach(function (key) {
+            if(input_users[key] == myname) return;
+            names += input_users[key] + ",";
+            name_num++;
+        });
+        if(name_num != 0) {
+            names = names.slice(0, -1) + "が入力しています。";
+            $('.input_now').text(names);
+        }else {
+            $('.input_now').text("");
+        }
     });
 
     $('#login').modal();
@@ -51,7 +94,6 @@ $(function() {
     });
 
     $('#submit_login').on('click', function() {
-        console.log($('#user_name').val());
         if($('#user_name').val() != "") {
             $('.error').css('display', 'none');
             $('body').removeClass('modal-open');
@@ -65,13 +107,48 @@ $(function() {
     });
 
     $('#m').change(function() {
-        // socket.emit('input end', myname);
+        if(isTyping) {
+            isTyping = false;
+            socket.emit('input end', myname);
+        }
     });
     $('#m').focusin(function() {
-        socket.emit('input start', myname);
-        console.log(myname);
+        if(!isTyping) {
+            isTyping = true;
+            socket.emit('input start', myname);
+        }
+        setTimeout(function() {
+            isTyping = false;
+            socket.emit('input end', myname);
+        }, 10000);
     });
     $('#m').focusout(function() {
-        // socket.emit('input end', myname);
+        if(isTyping) {
+            isTyping = false;
+            socket.emit('input end', myname);
+        }
+    });
+
+    $("#private_close").on('click', function(e) {
+        e.stopPropagation();
+        $("#private").animate({
+            bottom: "-465px"
+        }, 800);
+    });
+    $("#private_header").on('click', function() {
+        $("#private").animate({
+            bottom: "0"
+        }, 800);
+    });
+    $('#private_m').keypress(function(e) {
+        if(e.which == 13) {
+            console.log($(this).val());
+            socket.emit(private_id, $(this).val());
+            $(this).val("");
+            return false;
+        }
+    });
+    socket.on(myId, function() {
+
     });
 });
